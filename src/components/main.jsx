9fbccfb65b3ca22943521b731308ff438a0fa81b
Sesmic <sakahichauhan79@gmail.com>
2026-06-01 20:23:29 +0530

    Fixes: Number of die selected are properly kept track of. "Play Again" button works now.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useState,useEffect,useRef} from "react"
 import DieContainer from "./dieContainer.jsx"
 import RollDie from "./rollDie.jsx"
 
@@ -6,8 +6,19 @@ export default function Main(){
 
 
     const [Arr,setArr] = useState(getDieVal);
-
+    const [gameWon,setGameWon] = useState(false);
     const heldDice = Arr.filter(ele => ele.isHeld).length
+    const firstHeldValue = useRef(null);
+
+
+    useEffect(() =>
+    {
+        if(heldDice === 10 && Arr.every(ele => ele.value === firstHeldValue.current)){
+            setGameWon(prev=>!prev)
+        }
+        console.log(gameWon)
+    },[heldDice])
+
 
     function getDieVal(){
          return new Array(10)
@@ -26,27 +37,29 @@ export default function Main(){
         const temp = Arr.map(prev =>
         prev.isHeld ? prev : {...prev, value: Math.ceil(Math.random()*6)}
         )
-        console.log(temp)
         setArr(temp)
     }
 
-    function winGame(){
-        console.log(heldDice)
-        return heldDice === 10;
-    }
     function holdDice(ID){
-        winGame()
+        if(firstHeldValue.current == null){
+            firstHeldValue.current = Arr.find(die => die.isHeld)?.value
+        }
         setArr(prev => prev.map( die =>
             die.id === ID ? {...die, isHeld: !die.isHeld} : die
             )
         )
-        console.log(Arr)
+    }
+
+    function gameOver(){
+        setArr(getDieVal)
+        firstHeldValue.current = null;
+        setGameWon(prev => !prev)
     }
 
     return(
         <>
             <DieContainer dieProp={Arr} holdDice={holdDice}/>
-            <RollDie handelClick={rollDie}/>
+            <RollDie handelClick={rollDie} gameCondition={gameWon} gameOver={gameOver}/>
         </>
     )
 }
--- src/components/rollDie.jsx
+++ src/components/rollDie.jsx
@@ -5,8 +5,15 @@ export default function Roll(props){
 
     return(
         <>
-        <button onClick={props.handelClick}
-            className={"w-40 h-15 text-2xl font-bold text-white border-0 outline-0 rounded-md bg-indigo-600 hover:bg-indigo-700"}>Roll</button>
+            {
+                !props.gameCondition ?
+                <button onClick={props.handelClick}
+                className={"w-40 h-15 text-2xl font-bold text-white border-0 outline-0 rounded-md bg-indigo-600 hover:bg-indigo-700"}>Roll</button> :
+
+                <button
+                    onClick={props.gameOver}
+                    className={"w-40 h-15 text-2xl font-bold text-white border-0 outline-0 rounded-md bg-amber-600 hover:bg-amber-700"}>Play again</button>
+            }
         </>
     )
 }
